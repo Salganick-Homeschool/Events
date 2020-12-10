c14d1b1752a7da27ee55e28d2c184ebcbc1fd539
gabisavi727 <75393160+gabisavi727@users.noreply.github.com>
2020-12-10 11:25:19 -0700

    Update unscheduled.js

--- unscheduled.js
+++ unscheduled.js
@@ -7,7 +7,8 @@ const nickname = document.getElementById("nickname")
 const form = document.getElementById("unscheduledForm")
 const database = firebase.database();
 const rootRef = database.ref('recipients');
-
+const ticketNum = document.getElementById("howMany")
+let paymentAmount = ((ticketNum.value)*11)+((ticketNum.value)*9)+((ticketNum.value)*7)
 form.addEventListener("submit", (e) => {
     e.preventDefault()
     rootRef.child(fullName.value).set({
@@ -15,7 +16,11 @@ form.addEventListener("submit", (e) => {
         event: event.value,
         email: email.value,
         phoneNum: phone.value,
-
+        numberOfTickets: ticketNum.value,
+        cost: paymentAmount
     })
-   location = "EventsList.html" 
+    paymentAmount = ((ticketNum.value)*11)+((ticketNum.value)*9)+((ticketNum.value)*7)
+    console.log(paymentAmount+" kisses")
+    alert("It will be "+paymentAmount+" kisses when it is time for this "+event.value+". We will email you and/or text you when it is scheduled!")
+    location = "EventsList.html" 
 })
